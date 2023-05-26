1f110362acfbd05731ed3c8e3d6deddb1f751558
Artem <tema070@mail.ru>
2023-05-26 23:52:09 +0300

    begin

--- work_with_database.py
+++ work_with_database.py
@@ -5,26 +5,29 @@ cursor = conn.cursor()
 
 # conn.autocommit = True
 
-# # команда для создания базы данных metanit
+# # команда для создания базы данных
 # sql = "CREATE DATABASE raspisanie"
 #
 # # выполняем код sql
 # cursor.execute(sql)
 # print("База данных успешно создана")
 
-# # создаем таблицу аудиторий
-# cursor.execute("CREATE TABLE classroom (id SERIAL PRIMARY KEY, nomer INTEGER,  korpus INTEGER)")
+# # создаем таблицу
+# cursor.execute("CREATE TABLE classroom (id SERIAL PRIMARY KEY, nomer INTEGER, korpus INTEGER)")
+# cursor.execute("CREATE TABLE rasp_1 (classroom INTEGER NOT NULL, date DATE NOT NULL, num_pair INTEGER NOT NULL, "
+#                "predmet VARCHAR(200), teacher VARCHAR(200))")
+#
 # # поддверждаем транзакцию
 # conn.commit()
-# print("Таблица classroom успешно создана")
-
+# print("Таблица rasp_1 успешно создана")
+# cursor.execute("DROP TABLE classroom")
 # добавляем строку в таблицу people
-cursor.execute("INSERT INTO classroom (nomer, korpus) VALUES (430, 1)")
-cursor.execute("INSERT INTO classroom (nomer, korpus) VALUES (450, 1)")
-cursor.execute("INSERT INTO classroom (nomer, korpus) VALUES (454, 1)")
+# cursor.execute("INSERT INTO classroom (nomer, korpus) VALUES (430, 1)")
+# cursor.execute("INSERT INTO classroom (nomer, korpus) VALUES (450, 1)")
+# cursor.execute("INSERT INTO classroom (nomer, korpus) VALUES (454, 1)")
 # выполняем транзакцию
-conn.commit()
-print("Данные добавлены")
+# conn.commit()
+# print("Данные добавлены")
 
 cursor.close()
 conn.close()
